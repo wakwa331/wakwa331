dff33fac6b823e29e378fee0b50fecd0fc0b8f52
wakwa331 <pinobagas03@gmail.com>
2022-11-05 20:53:08 +0700

    Wm PinoMD

---
+++ config.js
@@ -0,0 +1,57 @@
+/**
+   * Create By THE JO BOT
+   * Base by Dika Ardnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'Your Key',
+}
+
+
+global.ownername = 'Pino'
+global.autorecording = true //status auto merekam ( auto record )
+global.autoketik = true //status auto mengetik (auto typing)
+global.available = true //status online (online)
+// Other
+global.owner = ['628891753051']
+global.premium = ['628891753051']
+global.packname = '©'
+global.author = 'Pino-MD'
+global.wlcm = []
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/jo.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
---
+++ src/menunya.js
@@ -0,0 +1,24 @@
+const menunya = `
+╭━━•›ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙‹•━━╮
+┃╭┈─────────────⩵꙰ཱི࿐
+┃╰── ⏤͟͟͞MENU NYA ──➤ ↶↷
+╰━━•›ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙‹•━━͙✩̣̣̣̣
+ ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬
+╭━━•›〘 ALL MENU 〙
+𖠀⩵꙰ཱི࿐ BUGMENU 
+𖠀⩵꙰ཱི࿐ FUNMENU 
+𖠀⩵꙰ཱི࿐ MAINMENU 
+𖠀⩵꙰ཱི࿐ VOICEMENU 
+𖠀⩵꙰ཱི࿐ GROUPMENU 
+𖠀⩵꙰ཱི࿐ OWNERMENU 
+𖠀⩵꙰ཱི࿐ ISLAMICMENU 
+𖠀⩵꙰ཱི࿐ TEKSPROMENU 
+𖠀⩵꙰ཱི࿐ CONVERTMENU 
+𖠀⩵꙰ཱི࿐ DOWNLOADMENU
+𖠀⩵꙰ཱི࿐ ANONYMOUSMENU
+╰━ ━ ━ ━ ━ ━ ━ ━ ━ ━•⩵꙰ཱི࿐
+ ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬
+
+© ⏤͟͟͞*A17-MD*
+`
+exports.menunya = menunya
